dcdd3616a6e21232c79a252bfbc2f982dd859e4e
Exac <contact@thomasmclennan.ca>
2013-12-25 14:52:34 -0800

    News Article

--- application/models/m_news_article.php
+++ application/models/m_news_article.php
@@ -5,13 +5,59 @@ class M_news_article extends CI_Model
 	public function __construct()
 	{
 		parent::__construct();
-		
-		//connect to database
 	}
 	
-	public function test($test)
+	public function get_article($article_id = 0)
 	{
-		return "<h1>{$test}</h1>";
+		return $this->get($article_id, "article");
+	}
+	
+	public function get_featured_image($article_id = 0)
+	{
+		return "/upload/news/".$this->get($article_id, "article_featured_image");
+	}
+	
+	public function get_title($article_id = 0)
+	{
+		return $this->get($article_id, "article_title");
+	}
+	
+	private function get($article_id, $col = "title")
+	{
+		$get_item = "";
+		
+		if($this->is_valid_article_id($article_id))
+		{
+			$query = $this->db->query("select {$col} from news where article_id = '{$article_id}' limit 1");
+		}else{
+			//get the newest article
+			$query = $this->db->query("select {$col} from news order by article_date desc limit 1");
+		}
+		
+		if($query->num_rows() > 0)
+		{
+			foreach($query->result() as $row)
+			{
+				$get_item .= reset($row); //print first item in array
+			}
+		}else{return 'Error: M_news_article->get() failed to return results.';}
+		
+		return $get_item;
+	}
+	
+	private function is_valid_article_id($article_id)
+	{
+		//if id = 0, get the newest article.
+		$query = $this->db->query("select count(*) as num_articles from news");
+		$row = $query->row();
+		
+		$num_articles = $row->num_articles;
+		
+		if(is_numeric($article_id) && $article_id > 0 && $article_id < $num_articles)
+		{
+			return true;
+		}
+		return false;
 	}
 }
 
