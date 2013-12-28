704f7f21ec646f2d4bcf641ed28cc2841b8a796c
Exac <contact@thomasmclennan.ca>
2013-12-27 23:55:43 -0800

    Added /forum.php

---
+++ application/controllers/forum.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Forum extends CI_Controller {
+	
+	public function index() #/forum/
+	{
+		$this->article();
+	}
+	
+	public function post($number = "0")
+	{
+		
+	}
+	
+	private function _get_article()
+	{
+		
+	}
+}
+
+/* End of file forum.php */
+/* Location: ./application/controllers/forum.php */
