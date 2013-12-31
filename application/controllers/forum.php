df7185e736541a48e0214475da59a7d9cfd4ee83
Exac <contact@thomasmclennan.ca>
2013-12-30 22:07:40 -0800

    Forum.php layout of index and post functions

--- application/controllers/forum.php
+++ application/controllers/forum.php
@@ -4,7 +4,28 @@ class Forum extends CI_Controller {
 	
 	public function index() #/forum/
 	{
-		echo "forum";
+		$this->load->view('v_temp_head');
+		
+		
+		
+		$this->load->view('v_forum', $this->_get_posts(30) );
+		
+		$this->load->view('v_temp_foot');
+	}
+	
+	public function post($post_id) #/forum/post/8
+	{
+		$this->load->view('v_temp_head');
+		$this->load->view('v_temp_foot');
+	}
+	
+	private function _get_posts()
+	{
+		$this->load->model('M_forum', '', TRUE);#load database
+		
+		$posts = ""; #retun this with HTML string for forum
+		
+		return $posts;
 	}
 }
 
