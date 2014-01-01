5a3fdbf5d06f8f060186afe9acd3f1b1bf2b9357
Exac <contact@thomasmclennan.ca>
2013-12-31 23:55:54 -0800

    take data from sql query

--- application/models/m_forum.php
+++ application/models/m_forum.php
@@ -9,11 +9,11 @@ class M_news_article extends CI_Model
 	
 	public function get_post_previews($num_posts)
 	{
-		for($p = /*current_post*/; )
+		$this->db->query("SELECT forum.title, forum.post_id, forum.last_reply, users.id, users.name FROM forum INNER JOIN users ON forum.author_id=users.id ORDER BY forum.last_reply DESC LIMIT {$num_posts}'");
+		foreach($query->result() as $row)
 		{
-			$this->db->query("SELECT forum.title, forum.post_id, forum.last_reply, users.id, users.name FROM forum INNER JOIN users ON forum.author_id=users.id ORDER BY forum.last_reply DESC LIMIT {$num_posts}'");
+			$item = reset($row);
 		}
-		
 	}
 	
 }
