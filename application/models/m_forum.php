2f710026561387da054565714b83bba86151c894
Exac <contact@thomasmclennan.ca>
2014-01-01 16:35:10 -0800

    Changing forum model to accept paging

--- application/models/m_forum.php
+++ application/models/m_forum.php
@@ -7,14 +7,19 @@ class M_forum extends CI_Model
 		parent::__construct();
 	}
 	
-	public function get_post_previews($num_posts)
+	public function get_post_previews($page, $num_posts)
 	{
 		$post_preview = '<ul class="post_preview">';
 		
-		$query = $this->db->query("SELECT forum.title, forum.post_id, forum.last_reply, users.id, users.name FROM forum INNER JOIN users ON forum.author_id=users.id ORDER BY forum.last_reply DESC LIMIT {$num_posts}");
+		if($page != 0)
+		{
+			
+		}
+		
+		$query = $this->db->query("SELECT forum.title, forum.post_id, forum.last_reply, SUBSTRING(forum.content, 1, 150) as content, users.id, users.name FROM forum INNER JOIN users ON forum.author_id=users.id ORDER BY forum.last_reply DESC LIMIT {$num_posts}");
 		foreach($query->result() as $row)
 		{
-			$post_preview .= "<li>". "<a href='/forum/post/{$row->post_id}'>" .$row->title."</a><span class='age'>".$this->human_timing(strtotime($row->last_reply))." old</span></li>";
+			$post_preview .= "<li>". "<a href='/forum/post/{$row->post_id}'>" .$row->title."</a><span class='age'>".$this->human_timing(strtotime($row->last_reply))." old</span><span class='excerpt'>".strip_tags($row->content)."...</span></li>";
 		}
 		$post_preview .= "</ul>";
 		
