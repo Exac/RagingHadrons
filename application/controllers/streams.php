441b5571cf5aa9ab951c1f350dab55a2b988c0e8
Exac <contact@thomasmclennan.ca>
2014-01-02 22:27:09 -0800

    Added  Streams controller

---
+++ application/controllers/streams.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Streams extends CI_Controller {
+	
+	public function index() #/streams/
+	{
+		
+	}
+	
+}
+
+/* End of file streams.php */
+/* Location: ./application/controllers/streams.php */
