b043e8bc8fc85cd4c20ae7212e2aaf9e4207e48a
Exac <contact@thomasmclennan.ca>
2014-01-03 21:11:34 -0800

    load model in streams controller

--- application/controllers/streams.php
+++ application/controllers/streams.php
@@ -5,6 +5,11 @@ class Streams extends CI_Controller {
 	public function index() #/streams/
 	{
 		$this->load->view('v_temp_head', array() );
+		
+		$this->load->model('M_streams', '', TRUE);#load database
+		$data['featured_stream'] = $this->M_streams->get_featured_stream();#get featured stream
+		//$data['stream_list'] = $this->M_streams->get_stream_list();#get RH listings
+		$this->load->view('v_streams', $data);#output to v_streams
 		$this->load->view('v_temp_foot');
 	}
 	
