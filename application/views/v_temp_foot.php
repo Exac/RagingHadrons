d0de5435cb22f2738a8b0088197d1addab5b4b60
Exac <contact@thomasmclennan.ca>
2014-01-04 17:32:40 -0800

    ION login setup

--- application/views/v_temp_foot.php
+++ application/views/v_temp_foot.php
@@ -1,4 +1,26 @@
-<footer> <span>&copy; <?php echo Date("Y"); ?> Raging Hadrons </span> </footer>
+<footer>
+	<span>&copy; <?php echo Date("Y"); ?> Raging Hadrons </span> 
+	<style type="text/css" scoped="scoped">
+		input[type=text],
+		input[type=password],
+		input[type=submit]{
+			background:black;
+			border:1px solid rgb(48,48,48);
+			color:#ffffff;
+			height:16px;
+			line-height:16px;
+			font-size:12px;
+			padding:1px;
+			box-sizing:content-box !important;
+			-moz-box-sizing:content-box !important;
+		}
+	</style>
+	<form action="/auth/" method="post" class="login">
+		<input type="text" name="identity" />
+		<input type="password" name="password" />
+		<input type="submit" value="login" />
+	</form>
+</footer>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
