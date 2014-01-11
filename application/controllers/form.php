68c0f5eb2c590b9537ccd4645c799293e6e9c2be
Exac <contact@thomasmclennan.ca>
2014-01-10 21:03:37 -0800

    Form Validation test

--- application/controllers/form.php
+++ application/controllers/form.php
@@ -5,6 +5,19 @@ class Form extends CI_Controller {
 	function index()
 	{
 		$this->load->helper(array('form', 'url'));
+		
+		$this->load->library('form_validation');
+		
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('form/news');
+		}
+		else
+		{
+			$this->load->view('formsuccess');
+		}
+
+		$this->load->helper(array('form', 'url'));
 
 		$this->load->library('form_validation');
 		
