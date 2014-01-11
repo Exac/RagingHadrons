5fe9f699869e9d361030cf378caae9723e609b44
Exac <contact@thomasmclennan.ca>
2014-01-10 23:51:08 -0800

    Form Validation test

--- application/controllers/form.php
+++ application/controllers/form.php
@@ -1,27 +1,38 @@
 <?php
 
 class Form extends CI_Controller {
+
+	/**
+	* FORM VALIDATION EXAMPLE
+	* $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[2]|max_length[16]|is_unique[users.username]|xss_clean|md5|matches[usernameconf]');
+	*/
 	
 	function index()
 	{
 		$this->load->helper(array('form', 'url'));
 		
 		$this->load->library('form_validation');
+
+		$this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[2]|max_length[16]|is_unique[users.username]|xss_clean');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[4]|max_length[40]|md5');
+		$this->form_validation->set_rules('passconf', 'Password Confirmation', 'required|matches[password]');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|max_length[99]|is_unique[users.email]');
 		
 		if ($this->form_validation->run() == FALSE)
 		{
-			$this->load->view('form/news');
+			$data['error'] = $this->form_validation;
+			$this->load->view('form/news', $data);
 		}
 		else
 		{
-			$this->load->view('formsuccess');
+			echo "SUCCESS";
 		}
 
-		$this->load->helper(array('form', 'url'));
+		// $this->load->helper(array('form', 'url'));
 
-		$this->load->library('form_validation');
+		// $this->load->library('form_validation');
 		
-		$this->load->view('form/news');
+		// $this->load->view('form/news');
 	}
 }
 ?>
