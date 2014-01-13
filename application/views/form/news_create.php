9444553dad80517540ca6190e85d1aff53e4ce41
Exac <contact@thomasmclennan.ca>
2014-01-12 20:50:15 -0800

    Display message for non-logged-in users

--- application/views/form/news_create.php
+++ application/views/form/news_create.php
@@ -2,9 +2,13 @@
 
 </style>
 
+<div class="notice">
 <?php echo validation_errors(); ?>
 
 <?php echo form_open('form/get/'.$location); ?>
+
+<?php if(isset($errors))echo $errors; ?>
+</div>
 
 <h5>Title</h5>
 <input type="text" name="title" value="<?php echo set_value('title'); ?>" size="50" />
