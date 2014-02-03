15c637a848bbd981f3980ca6e0102615d6a4b465
Exac <contact@thomasmclennan.ca>
2014-02-02 21:48:16 -0800

    get_news_list

--- application/models/m_maingrid_factory.php
+++ application/models/m_maingrid_factory.php
@@ -9,7 +9,20 @@ class M_maingrid_factory extends CI_Model
 	
 	public function get_news_list($item_quantity = 7)
 	{
-		return "";
+		$news_list = "";
+		$query = $this->db->query("select article_id, article_title from news order by article_date desc limit 7");
+		if($query->num_rows() > 0)
+		{
+			foreach($query->result() as $row)
+			{
+				$news_list .= "<li><a href='/news/article/"
+						   .$row['article_id']
+						   ."'>"
+						   .$row['article_title']
+						   ."</a></li>\n";
+			}
+		}
+		return $news_list;
 	}
 
 	public function get_posts_list($item_quantity = 4)
